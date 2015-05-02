003ebf49ecf28e8f58a471e94ac7c5965f80255d
Ohad Partuck 1 <ohadpartuck@gmail.com>
2015-05-02 16:06:30 +0300

    cloud code

--- MyCloudCode/cloud/challenges_helper.js
+++ MyCloudCode/cloud/challenges_helper.js
@@ -1,11 +1,14 @@
-var _ = require('underscore');
 
 function did_user_completed_all_theme_challenges(user_completed_challenges, theme_challenges){
-		var completed_challenge_ids = _.pluck(user_completed_challenges, "id");
+		var completed_challenge_ids = Object.keys(user.attributes.challenges_completed);
 		res = theme_challenges.filter(function (theme_challenge) {
-    			return user_completed_challenges.indexOf(theme_challenge) > -1;
-		}).length == arr1.length
+    			return completed_challenge_ids.indexOf(theme_challenge) > -1;
+		}).length == completed_challenge_ids.length
 		return res;
+}
+
+function completed_task_payload(){
+	return {"created_at": new Date().getTime()}
 }
 
 exports.is_already_completed = function(user_completed_challenges_object, challenge_id) {
@@ -15,27 +18,35 @@ exports.is_already_completed = function(user_completed_challenges_object, challe
 
 exports.mark_challenge_completed = function(user, challenge){
   
-	var user_data = user.attributes,
-	challenge_data = challenge.attributes;
-	user_data.challenges_completed = user_data.challenges_completed || {};
-	user_data.challenges_completed.add(challenge);
-	
+	var challenges_completed = user.attributes.challenges_completed || {},
+	themes_completed = user.attributes.themes_completed || {};
 
+	challenges_completed[challenge.id] = completed_task_payload();
+	user.set("challenges_completed", challenges_completed)
+	console.log("111");
+	console.log(challenges_completed);
+	user.save();
+	
 	//update user points
-	user_data.points = user_data.points || 0;
-	user_data.points += challenge_data.points;
+	var points = user.attributes.points || 0;
+	user.set("points", points +  parseInt(challenge.attributes.points));
 
 	//update user completed themes, id needed
-	challenge_data.themes.forEach(function(theme){
+	challenge.attributes.themes.forEach(function(theme){
 		var theme_challenges = theme.attributes.challenges;
 		//check if needs to update the themes completed for the user
-		if (did_user_completed_all_theme_challenges(user_data.challenges_completed, theme_challenges)){
-			user_data.themes_completed.add(theme);
+		if (did_user_completed_all_theme_challenges(user.attributes.challenges_completed, theme_challenges)){
+			//actually update in the user model	
+			themes_completed[theme.id] = {"created_at": new Date().getTime(), "badge": theme.badge};
+			user.set("themes_completed", themes_completed);
+				console.log("222");
+				console.log(themes_completed);
 		}
 
 	});
 
 	user.save();
+	
 
 }
 
--- MyCloudCode/cloud/main.js
+++ MyCloudCode/cloud/main.js
@@ -3,8 +3,8 @@ var challenges_helper = require('cloud/challenges_helper.js');
 Parse.Cloud.define("mark_challenge_completed", function(request, response) {
 
    var user = Parse.User.current(),
-   challenge_id = request.params.challenge_id ,
-   challenges_completed = user.attributes.challenges_completed || {}
+   challenge_id = request.params.challenge_id;
+   challenges_completed = user.attributes.challenges_completed || {},
    already_completed = challenges_helper.is_already_completed(challenges_completed, challenge_id);
    
    if (already_completed){
@@ -25,7 +25,7 @@ Parse.Cloud.define("mark_challenge_completed", function(request, response) {
 			    
 			  },
 			  error: function(error) {
-			    alert("Error: " + error.code + " " + error.message);
+			    alert(error);
 			  }
 		});
    	   
